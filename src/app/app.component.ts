5e7cf6feb0fb19316a1163d61d06dc7c483fc388
sanjusiva <sanjusiru123@gmail.com>
2022-04-21 16:57:41 +0530

    pipes

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pipe';
+  name="peTer parKer";
+  thisDay=Date.now();
+  word="Aspire Systems";
+  money=500;
+}
